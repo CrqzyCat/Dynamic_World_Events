c8e96f2ba1b3c1bd39eb1077a53ca3bd44d8d023
crqzycat <4128@htl.rennweg.at>
2026-05-08 09:59:36 +0200

    Initial commit

---
+++ src/main/java/dynamic_world_events/dynamic_World_Events/Dynamic_World_Events.java
@@ -0,0 +1,16 @@
+package dynamic_world_events.dynamic_World_Events;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Dynamic_World_Events extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
